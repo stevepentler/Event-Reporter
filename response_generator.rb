b3e09c05a2b3cc7d8ce191d3a5d6a5991785bfd7
Steve Pentler <stevepentler@gmail.com>
2015-12-29 21:44:56 -0600

    successful calls on sample file

--- response_generator.rb
+++ response_generator.rb
@@ -1,18 +1,25 @@
 require_relative 'file_reader'
+require_relative 'queue'
 require 'pry'
 
 class ResponseGenerator
+  def intialize
+  end
 
   def generate_response(user_input)
 
   end 
 
-  def load_file(file_name = event_attendees.csv)
-    FileReader.read(file_name)
-
+  def load_file(file_name = 'event_attendees.csv')
+    @data = FileReader.new.read(file_name)
+    # @data.count
+    # @data.each do |f|
+    #   puts f[:first_name]
+    # end
   end 
 
   def help
+
   end 
 
   def help_command
@@ -39,18 +46,7 @@ class ResponseGenerator
   def find(attribute, criteria)
   end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end 
+
+c = ResponseGenerator.new
+c.load_file('sample_attendees.csv')
