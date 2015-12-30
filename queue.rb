e0225fa9150a09e95518a3fd347f5a665a79a73f
Steve Pentler <stevepentler@gmail.com>
2015-12-29 21:52:08 -0600

    instantiated queue class

--- queue.rb
+++ queue.rb
@@ -1,3 +1,27 @@
 class Queue
+  def initialize
+    @count = 0
+  end
+
+  # def queue_count
+  # end
+
+  def queue_clear
+    @count = 0 
+    @reponse = nil
+  end
+
+  def queue_print
+    
+  end
+
+  def queue_print_by(attribute)
+  end
+
+  def queue_save(file_name = event_attendees.csv)
+  end
+
+  def find(attribute, criteria)
+  end
 
 end 
